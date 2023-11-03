312f614b116b40c26367b2b0d75a3678f7b54678
jacobdenobel <jacobdenobel@gmail.com>
2023-11-03 13:39:54 +0100

    ars

---
+++ algorithms/ars.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+
+import numpy as np
+import ioh
+
+from .algorithm import Algorithm, SolutionType, DEFAULT_MAX_BUDGET
+
+
+@dataclass
+class ARSV1(Algorithm):
+    budget: int = DEFAULT_MAX_BUDGET
+    sigma0: float = 0.02     # learning rate alpha
+    lambda_: int = 16        # n offspring for each direction
+    mu: int = 16             # best offspring
+    eta: float = 0.03        # noise parameter  
+    
+    def __call__(self, problem: ioh.ProblemType) -> SolutionType:
+        n = problem.meta_data.n_variables
+        x_prime = np.zeros((n, 1))
+
+        sigma = self.sigma0
+        try:
+            while not self.should_terminate(problem, self.lambda_):
+                delta = np.random.normal(size=(self.n, self.lambda_))
+
+                neg = x_prime - (self.eta * delta)
+                pos = x_prime + (self.eta * delta)
+
+                neg_reward = -problem(neg)
+                pos_reward = -problem(pos)
+
+                best_rewards = np.maximum(neg_reward, pos_reward)
+                idx = np.argsort(best_rewards)[::-1]
+
+                f = np.r_[neg_reward, pos_reward]
+                sigma_rewards = f.std() + 1e-12
+                weight = self.sigma0 / (self.lambda_ * sigma_rewards)
+
+                delta_rewards = pos_reward - neg_reward
+                x_prime += (weight * (delta_rewards[idx] * delta[:, idx]).sum(axis=1, keepdims=True))
+
+        except KeyboardInterrupt:
+            pass
+        return x_prime
--- algorithms/egs.py
+++ algorithms/egs.py
@@ -15,19 +15,20 @@ class EGS(Algorithm):
     kappa: float = 2.0    
     
     def __call__(self, problem: ioh.ProblemType) -> SolutionType:
-        x_prime = np.zeros((self.n, 1))
+        n = problem.meta_data.n_variables
+        x_prime = np.zeros((n, 1))
 
         sigma = self.sigma0
         try:
             while not self.should_terminate(problem, self.lambda_):
-                Z = np.random.normal(size=(self.n, self.lambda_))
+                Z = np.random.normal(size=(n, self.lambda_))
                 y_pos = x_prime + sigma * Z
                 y_neg = x_prime - sigma * Z
                 f_pos = problem(y_pos)
                 f_neg = problem(y_neg)
 
                 z_avg  = np.sum((f_neg - f_pos) * Z, axis=1, keepdims=True)
-                z_prog = (np.sqrt(self.n) / self.kappa) * (z_avg / np.linalg.norm(z_avg))
+                z_prog = (np.sqrt(n) / self.kappa) * (z_avg / np.linalg.norm(z_avg))
                 x_prime = x_prime + sigma * z_prog
 
         except KeyboardInterrupt:
